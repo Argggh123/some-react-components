786544d1c0fc43c3fe59e89436e975220a491b40
g.valeev <g.valeev@xsolla.com>
2020-11-19 09:45:07 +0500

    add gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const { src, dest, series } = require('gulp');
+const { createProject } = require('gulp-typescript');
+
+const packages = {
+  common: './packages/common',
+  crm: './packages/ui',
+};
+
+const copyPackage = (pathToPackage) => () =>
+  src(path.resolve(__dirname, pathToPackage, 'package.json')).pipe(dest(path.resolve(__dirname, pathToPackage, 'dist')));
+
+const buildPackage = (projectFolder) => () => {
+  const project = createProject(path.resolve(__dirname, projectFolder, 'tsconfig.json'));
+  return project.src().pipe(project()).pipe(dest(path.resolve(__dirname, projectFolder, 'dist')));
+};
+
+const buildCommon = series(
+  buildPackage(packages.common),
+  copyPackage(packages.common),
+);
+const buildCrm = series(
+  buildPackage(packages.crm),
+  copyPackage(packages.crm),
+);
+
+exports.crm = buildCrm;
+exports.common = buildCommon;
+
+exports.build = series(buildCommon, buildCrm);
